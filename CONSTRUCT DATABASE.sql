66701a749c17431ee209c0a971ba1cc62b0902df
Tobias Laving <tobbeman1994@gmail.com>
2017-03-04 11:52:08 +0100

    Buyer can build road from wherever, not good

--- CONSTRUCT DATABASE.sql
+++ CONSTRUCT DATABASE.sql
@@ -69,7 +69,177 @@ CREATE VIEW NextMoves AS
 SELECT persons.country, persons.personnumber, A1.country AS currentCountry, A1.name AS currentArea, A2.country AS validCountry, A2.name AS validArea, roads.roadtax
 FROM persons, areas A1, areas A2, roads
 WHERE persons.locationcountry = A1.country AND persons.locationarea = A1.name AND roads.fromcountry = A1.country AND roads.fromarea = A1.name AND roads.tocountry = A2.country AND roads.toarea = A2.name AND roads.roadtax < persons.budget;
+
+DROP VIEW IF EXISTS AssetSummery;
+CREATE VIEW AssertSummery AS
+SELECT p.country, p.personnumber, p.budget, (SELECT COUNT(hotels.name) * getval('hotelprice') FROM hotels WHERE hotels.ownercountry = p.country AND hotels.ownerpersonnumber = p.personnumber) + (SELECT COUNT(roads.roadtax) * getval('roadprice') FROM roads WHERE roads.ownercountry = p.country AND roads.ownerpersonnumber = p.personnumber) AS assets, (SELECT COUNT(hotels.name) * getval('hotelprice') * getval('hotelrefund') FROM hotels WHERE hotels.ownercountry = p.country AND hotels.ownerpersonnumber = p.personnumber) AS reclaimable
+FROM persons p;
 --end Create Views--
+--Create Triggers--
+CREATE OR REPLACE FUNCTION check_road() RETURNS TRIGGER AS $$
+
+BEGIN
+
+	IF(TG_OP = 'INSERT') THEN
+
+		IF(SELECT EXISTS(SELECT 1 FROM ROADS WHERE
+				(((fromcountry = NEW.fromcountry AND fromarea = NEW.fromarea) AND (tocountry = NEW.tocountry AND toarea = NEW.toarea))
+					OR
+				((fromcountry = NEW.tocountry AND fromarea = NEW.toarea) AND (tocountry = NEW.fromcountry AND toarea = NEW.fromarea)))
+					AND
+				(ownercountry = NEW.ownercountry AND ownerpersonnumber = NEW.ownerpersonnumber)
+		)) THEN
+			RAISE EXCEPTION 'Owner owns a road between these areas already'
+				USING HINT = '';
+		END IF;
+
+		IF(SELECT EXISTS(SELECT 1 FROM Persons WHERE
+			(
+       ((country = NEW.ownercountry AND personnumber = NEW.ownerpersonnumber)
+				AND
+				(
+					(locationcountry = NEW.tocountry AND locationarea = NEW.toarea)
+					AND
+					(locationcountry = NEW.fromcountry AND locationarea = NEW.fromarea)
+				))
+        OR
+        (country = ' ' AND personnumber = ' ')
+			)
+			)) THEN
+      ELSE
+				RAISE EXCEPTION 'The buyer of the road must be at the area of construction'
+					USING HINT = '';
+		END IF;
+
+		--Check persons budget and throw exception if its to low
+		IF((SELECT budget FROM Persons WHERE personnumber = NEW.ownerpersonnumber AND country = NEW.ownercountry) < getval('roadprice')) THEN
+			RAISE EXCEPTION 'The buyer of the road can not afford the road'
+				USING HINT = '';
+		END IF;
+
+		RETURN NEW;
+	END IF;
+
+
+
+	--DELETE BOTH WAYS
+	IF(TG_OP = 'DELETE') THEN
+		--CHECK IF THE WAY IS CORRECT
+		IF(
+			ownercountry = OLD.ownercountry AND ownerpersonnumber = OLD.ownerpersonnumber
+			AND
+			OLD.fromcountry = fromcountry AND OLD.fromarea = fromarea AND OLD.tocountry = tocountry AND OLD.toarea = toarea
+			)THEN
+				RETURN OLD;
+		END IF;
+
+		--DELETE THE OTHER WAY IF THE FIRST IS NOT FOUND
+		DELETE FROM Roads WHERE ownercountry = OLD.ownercountry AND ownerpersonnumber = OLD.ownerpersonnumber AND OLD.tocountry = fromcountry AND OLD.toarea = fromarea AND OLD.fromcountry = tocountry AND OLD.fromarea = toarea;
+	END IF;
+
+	-- TODO
+	--IF(TG_OP = 'UPDATE') THEN
+		--IF(	OLD.fromcountry != NEW.fromcountry OR
+		--	OLD.tocountry != NEW.tocountry OR
+		--	OLD.ownercountry != NEW.ownercountry OR
+		--	OLD.ownerpersonnumber != NEW.owner.personnumber) THEN
+		--		RAISE EXCEPTION 'Cannot change owner/directions on a road'
+		--END IF;
+		--RETURN NEW;
+	--END IF;
+
+
+END;
+
+$$ LANGUAGE 'plpgsql';
+
+DROP TRIGGER IF EXISTS check_road ON Roads;
+CREATE TRIGGER check_road BEFORE INSERT OR UPDATE ON Roads
+    FOR EACH ROW EXECUTE PROCEDURE check_road();
+
+
+------------------------------------------------ PERSON-------------------------------------------------
+
+CREATE OR REPLACE FUNCTION check_person() RETURNS TRIGGER AS $$
+DECLARE
+	minroadtax integer;
+	numHotels integer;
+	moved boolean = false;
+BEGIN
+
+  IF(SELECT EXISTS(SELECT country,name FROM Areas WHERE country=NEW.locationcountry AND name = NEW.locationarea))THEN
+  ELSE
+  	IF (SELECT EXISTS(SELECT ownercountry,ownerpersonnumber FROM Roads
+  		WHERE ((fromarea = NEW.locationarea AND fromcountry = NEW.locationcountry) OR (toarea = NEW.locationarea AND tocountry = NEW.locationcountry)) AND (ownercountry = NEW.country AND ownerpersonnumber = NEW.personnumber )))
+  		THEN
+  		--UPDATE Persons SET locationarea = NEW.locationarea, locationcountry = NEW.locationcountry
+  				--WHERE country = NEW.country AND personnumber = NEW.personnumber;
+  				moved = true;
+
+  	ELSIF(SELECT EXISTS(SELECT roadtax FROM Roads
+  		WHERE  (fromarea = NEW.locationarea AND fromcountry = NEW.locationcountry) OR (toarea = NEW.locationarea AND tocountry = NEW.locationcountry)
+  		ORDER BY roadtax ASC)LIMIT 1) THEN
+  			NEW.budget = NEW.budget - roadtax;
+  			moved = true;
+  	ELSE
+  		RAISE EXCEPTION 'No road to destination';
+
+  	END IF;
+
+  	--Check hotel--
+  	IF(moved) THEN
+  			--Check hotel--
+  			IF(SELECT EXISTS(SELECT COUNT(Hotels.name) INTO numHotels  FROM Hotels
+  				WHERE locationcountry = NEW.locationcountry AND locationname = NEW.locationarea
+  				))THEN
+  					NEW.budget = NEW.budget + (getval('cityvisit')/numHotels);
+  			END IF;
+  	END IF;
+  END IF;
+	RETURN NEW;
+END;
+
+$$ LANGUAGE 'plpgsql';
+
+DROP TRIGGER IF EXISTS check_person ON Persons;
+CREATE TRIGGER check_person BEFORE UPDATE ON Persons
+    FOR EACH ROW EXECUTE PROCEDURE check_person();
+
+--------------------------------------------HOTELS------------------------------------------
+CREATE OR REPLACE FUNCTION check_hotel() RETURNS TRIGGER AS $$
+
+BEGIN
+	IF(TG_OP = 'INSERT') THEN
+		--Check that the player buying do not own an hotel in the same area--
+		IF EXISTS(SELECT name FROM Hotels WHERE ownercountry = NEW.ownercountry AND ownerpersonnumber = NEW.ownerpersonnumber AND locationname = NEW.locationname AND locationcountry = NEW.locationcountry)THEN
+		RAISE EXCEPTION 'haha';
+		ELSE
+			UPDATE Persons SET budget = budget - getval('hotelprice') WHERE country = NEW.ownercountry AND personnumber = NEW.ownerpersonnumber;
+		END IF;
+	END IF;
+
+	IF(TG_OP = 'UPDATE') THEN
+		--Check that locaion does not change--
+		IF NOT EXISTS(SELECT name FROM Hotels WHERE locationname = NEW.locationname AND locationcountry = NEW.locationcountry AND name = NEW.name)THEN
+			RAISE EXCEPTION 'Hotel does not exist!';
+		END IF;
+	END IF;
+
+	IF(TG_OP = 'UPDATE') THEN
+		--Update persons budget
+		UPDATE Persons SET budget = budget + (getval('hotelprice') * getval('hotelrefund')) WHERE country = NEW.ownercountry AND personnumber = NEW.ownerpersonnumber;
+	END IF;
+
+	RETURN NEW;
+END;
+
+$$ LANGUAGE 'plpgsql';
+
+DROP TRIGGER IF EXISTS check_hotel ON Hotels;
+CREATE TRIGGER check_hotel BEFORE INSERT OR UPDATE OR DELETE ON Hotels
+    FOR EACH ROW EXECUTE PROCEDURE check_hotel();
+
+--end Create Triggers--
 --Fill database--
 
 --MUST HAVE--
@@ -98,11 +268,8 @@ INSERT INTO Hotels VALUES('Hotel', 'Sweden', 'Visby', 'Sweden', '970221-4555');
 
 INSERT INTO Roads VALUES ('Sweden', 'Gothenburg', 'Sweden', 'Stockholm', ' ', ' ', 10);
 INSERT INTO Roads VALUES ('Sweden', 'Gothenburg', 'Sweden', 'Stockholm', 'Sweden', '940606-6952', 15);
---INSERT INTO Roads VALUES ('Sweden', 'Stockholm', 'Sweden', 'Gothenburg', 'Sweden', '940606-6952', 15);
+INSERT INTO Roads VALUES ('Sweden', 'Stockholm', 'Sweden', 'Visby', 'Sweden', '940606-6952', 15);
 
 
 
 --end Fill database--
-
-
-
