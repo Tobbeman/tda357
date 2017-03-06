35f734cce0c26bd97fdffbdb5d19a1668134a982
Tobias Laving <tobbeman1994@gmail.com>
2017-03-06 15:47:02 +0100

    Cleaned contruct

--- CONSTRUCT DATABASE.sql
+++ CONSTRUCT DATABASE.sql
@@ -268,26 +268,3 @@ INSERT INTO Persons VALUES ( ' ' , ' ' , 'The Government' , ' ' , ' ' , 100000) 
 
 
 --end Fill database--
-
---TEST--
-
---SETUP
-INSERT INTO Countries VALUES ( 'testCountry1') ;
-INSERT INTO Areas VALUES ( 'testCountry1' , 'testCity1' , 491630) ;
-INSERT INTO Areas VALUES ( 'testCountry1' , 'testCity2', 1337) ;
-INSERT INTO Areas VALUES ( 'testCountry1' , 'testCity3', 1337) ;
-
-INSERT INTO Cities VALUES ('testCountry1' , 'testCity1', 491630);
-INSERT INTO Persons VALUES ('testCountry1', '123456-1234', 'Fisken allan', 'testCountry1', 'testCity2', 999999);
-INSERT INTO Persons VALUES ('testCountry1', '133456-1234', 'Fiddsken allan', 'testCountry1', 'testCity2', 9999);
-INSERT INTO Persons VALUES ('testCountry1', '133456-1244', 'Fiddsken allan', 'testCountry1', 'testCity2', 9999);
-
-INSERT INTO Hotels VALUES('Hotel', 'testCountry1', 'testCity1', 'testCountry1', '133456-1234');
-INSERT INTO Hotels VALUES('Hotel', 'testCountry1', 'testCity1', 'testCountry1', '133456-1244');
-
-INSERT INTO Roads VALUES ('testCountry1', 'testCity1', 'testCountry1', 'testCity2', ' ' , ' ', 1);
-
-
---TEST
-
-UPDATE Persons SET locationarea = 'testCity1' WHERE personnumber = '123456-1234' ;
