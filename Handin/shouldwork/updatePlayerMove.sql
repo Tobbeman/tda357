3a081540947043bb768a8fd4dc15fd9f749fe7f3
Tobias Laving <tobbeman1994@gmail.com>
2017-03-13 14:03:48 +0100

    Now labb 2 is ready for handin

--- Handin/shouldwork/updatePlayerMove.sql
+++ Handin/shouldwork/updatePlayerMove.sql
@@ -11,13 +11,13 @@ INSERT INTO Areas VALUES ( 'testCountry1' , 'testCity3', 1337) ;
 INSERT INTO Cities VALUES ('testCountry1' , 'testCity1', 491630);
 INSERT INTO Persons VALUES ('testCountry1', '123456-1234', 'Fisken allan', 'testCountry1', 'testCity1', 99999999);
 
-INSERT INTO Roads VALUES ('testCountry1', 'testCity1', 'testCountry1', 'testCity2', 'testCity1' , '123456-1234', 15);
+INSERT INTO Roads VALUES ('testCountry1', 'testCity1', 'testCountry1', 'testCity2', 'testCountry1' , '123456-1234', 15);
 
 
 --TEST
 
-UPDATE Persons SET locationarea = 'testCity1' WHERE country != ' ';
-UPDATE Persons SET locationarea = 'testCity2' WHERE country != ' ';
+UPDATE Persons SET locationarea = 'testCity1';
+UPDATE Persons SET locationarea = 'testCity2';
 
 --TEST--
 
